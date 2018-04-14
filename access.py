a70d083aaa8f228484b93b93479560bbf3ad1a44
mattwitkow <mattwitkow@gmail.com>
2018-04-14 13:56:21 -0400

    more commands

--- access.py
+++ access.py
@@ -8,11 +8,45 @@ if not filename[-4:] == '.txt':
     sys.exit(0)
 
 
+f = open(filename, 'r')
+lines = f.readlines()
+
+class loginState:
+    def __init__self(self, login, logout):#probably won't need logout, may be useful for audit logs
+        self.login = login
+        self.logout = logout
 
 
+class User:
+    def __init__(self, isRoot, username, password):
+        self.isRoot = isRoot
+        self.username = username
+        self.password = password
+        self.loggedIn = False
+
+class File:
+    def __init__(self, fileName, owner, perms1, perms2, perms3, groupName):
+        self.isRoot = fileName
+        self.owner = owner
+        self.perms1 = perms1
+        self.perms2 = perms2
+        self.perms3 = perms3
+        self.groupName = groupName
+
+class Group:#add check later to snsure no duplicate groups
+    def __init__(self, groupName):
+       self.userList = list()
+       self.groupName = groupName
 
 
-#Define command list
+#holds what user is currently logged in
+loggedIn = loginState()
+#holds the group objects
+groupList = list()
+#holds the files
+fileList = list()
+#holds the users
+userList = list()
 
 #useradd username password
 #login username password
@@ -31,36 +65,25 @@ if not filename[-4:] == '.txt':
 #In order to create the super user for the system, the very first line of the instructions file has to be of
 #the form:
 #useradd root password
-f = open('filename', 'r')
-lines = f.readlines()
 
+def checkGroupDup(groupNameIn):
+    for groupCheck in groupList:
+        if groupCheck.groupName == groupNameIn:
+            return True
+    return False
+def checkFileDup(fileNameIn):
+    for fileCheck in fileList:
+        if fileCheck.fileName == fileNameIn:
+            return True
+    return False
 
-
-class User:
-    def __init__(self, isRoot, username, password):
-        self.isRoot = isRoot
-        self.username = username
-        self.password = password
-
-class File:
-    def __init__(self, fileName, owner, perms1, perms2, perms3, groupName):
-        self.isRoot = fileName
-        self.owner = owner
-        self.perms1 = perms1
-        self.perms2 = perms2
-        self.perms3 = perms3
-        self.groupName = groupName
-
-class Group:#add check later to snsure no duplicate groups
-    def __init__(self, userList):
-       self.userList = userList
 
 #method that takes in list of user commands and creates global simplified commands list
-def makeCMDList(self, lines): #TODO make sure command to create root user is the first line
+def makeCMDList(lines): #TODO make sure command to create root user is the first line
     for line in lines:
-        line = line.strip().trim
+        line = line.strip()
         argList = line.split()
-        argCount = argList.size()
+        argCount = len(argList)
 
         # group ifs by the argument count.
         if argCount > 5 or argCount < 1:
@@ -69,11 +92,26 @@ def makeCMDList(self, lines): #TODO make sure command to create root user is the
             if(line.equals("logout")):
                 #TODO
                 return
-            if(line.equals("end")):
+            elif(line.equals("end")):
                 #TODO
                 return
-    return
+            else:
+                print("Invalid arguments. Unrecognized command: " + line)
+                return
+
+        if argCount == 2:
+            if argList[0] == "groupadd" and (argList[1] is not None and len(argList[1]) < 30 and argList[1] != "nil"
+                                             and not checkGroupDup(argList[1])):
+                #TODO
+                return
+            if argList[0] == "mkfile" and (loggedIn.login is not None and not checkFileDup(arlList[1])):
+                #TODO
+                return
+
+
+        return
         #keep going up to 5
+makeCMDList(lines)
 
 
 
