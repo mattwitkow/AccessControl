f1903759c5e01b762b1fc4d0e9e6dfe727a96a6e
mattwitkow <mattwitkow@gmail.com>
2018-04-17 08:16:08 -0400

    Needs docs

--- access.py
+++ access.py
@@ -220,20 +220,57 @@ def fillWrite(splitlist):
         stringyboi += splitlist[i] + " "
     return stringyboi
 
+def retGroupUsers(groupName):
+    group = getGroup(groupName)
+    retstring = ''
+    for entry in group.userList:
+        retstring += entry.username + " "
+    return retstring
+
+
+def writeGroup():
+    gFile = open("groups.txt","a")
+    if gFile is None:
+        print "GROUPS file never found!! HELP!!"
+    print "GROUPS "+ "\n"
+    for entry in groupList:
+        print entry.groupName + ": "+retGroupUsers(entry.groupName) + " "
+        gFile.write(entry.groupName + ": "+retGroupUsers(entry.groupName)+ " " + "/n")
+
+def writeFiles():
+    fFile = open("files.txt", "a")
+    if fFile is None:
+        print "FILES file never found!! HELP!!"
+    print "FILES " + "\n"
+    for file in fileList:
+        print file.fileName +" "+file.owner.username+ " " + file.perms1 + " " + file.perms2 + " " + file.perms3
+        fFile.write(file.fileName +" "+file.owner.username+ " " + file.perms1 + " " + file.perms2 + " " + file.perms3 + "/n")
+
+
 
 #method that takes in list of user commands and creates global simplified commands list
 def makeCMDList(lines): #TODO make sure command to create root user is the first line
     linecount = 0
     for line in lines:
+
         linecount += 1
+        if line == "end":
+            auditFile.write("end")
+            writeGroup()
+            writeFiles()
+            return
         line = line.strip()
         argList = line.split()
         argCount = len(argList)
 
         # group ifs by the argument count.
         if argList[0] != "write" and (argCount > 5 or argCount < 1) or argCount == 4:
-                print("Invalid arguments. Incorrect count of clauses at command: " + line)
-                return
+
+            auditFile.write(
+                "Invalid arguments or command never ran due to no user being logged in, invalid permissions, etc: " + line + "\n")
+            print(
+                        "Invalid arguments or command never ran due to no user being logged in, invalid permissions, etc: " + line + "\n")
+                #return
         elif argList[0] == "useradd" and linecount == 1 and argList[1] == "root" and argCount == 3:
             print("root user created\n")
             auditFile.write("root user created\n")
@@ -258,75 +295,91 @@ def makeCMDList(lines): #TODO make sure command to create root user is the first
                 print("user logout " +outtie +"\n")
                 #TODO
             #    return
-            elif line == "end" and not loggedIn.logLock:
 
-                print("end\n")
-                return
+
+
+
                 #TODO
              #   return
             else:
-                print("Invalid arguments. Unrecognized command: " + line + "\n")
-                return
+                auditFile.write(
+                    "Invalid arguments or command never ran due to no user being logged in, invalid permissions, etc: " + line + "\n")
+                print(
+                            "Invalid arguments or command never ran due to no user being logged in, invalid permissions, etc: " + line + "\n")
 
         elif argCount == 2:
             if argList[0] == "groupadd" and not loggedIn.logLock and (argList[1] is not None
                                             and len(argList[1]) < 30 and argList[1] != "nil"
                                              and not checkGroupDup(argList[1])):
+
                 groupGuy = Group(argList[1])
                 groupList.add(groupGuy)
-                print("groupadd\n")
+                auditFile.write("group " + argList[1] + " made " "\n")
+                print "group " + argList[1] + " made " "\n"
                 #TODO
               #  return
             elif argList[0] == "mkfile" and not loggedIn.logLock and (loggedIn.login is not None and not checkFileDup(argList[1])):
 
                 madeFile = File(argList[1], loggedIn.login, "rw-","---","---")
                 fileList.add(madeFile)
+                auditFile.write("file " + argList[1] + " made with owner " + loggedIn.login.username +" made with default permissions" "\n")
+                print "file " + argList[1] + " made with owner " + loggedIn.login.username +" made with default permissions" "\n"
 
-                print("mkfile\n")
+
                 #TODO
                # return
             elif argList[0] == "read" and not loggedIn.logLock and (checkFileDup(argList[1])): #Check permissions later
                 if checkCanRead(loggedIn.login.username, argList[1]):
-                    print "cool, read" + argList[1]
-                print("readfile\n")
+                    auditFile.write("User " + loggedIn.login.username + " read " +argList[1] +" as " + fillWrite(argList)  +"\n")
+                    print "User " + loggedIn.login.username + " read " +argList[1] +" as " + fillWrite(argList)  +"\n"
+                else:
+                    auditFile.write("User " + loggedIn.login.username + " denied read access of" + argList[1]+ "\n")
+                    print "User " + loggedIn.login.username + " denied read access of" + argList[1]+ "\n"
+
                 #TODO
                 #return
             elif argList[0] == "execute" and not loggedIn.logLock and (checkFileDup(argList[1])): #Check permissions later
                 if checkCanExecute(loggedIn.login.username, argList[1]):
-                    print "cool, executed" + argList[1]
-                print("execute\n")
-                #TODO
-                #return
-            elif argList[0] == "ls" and not loggedIn.logLock and (checkFileDup(argList[1])): #Check permissions later
-                #TODO
-                print("ls\n")
-                #return
+                    auditFile.write("User " + loggedIn.login.username + " executes " +argList[1] +" as " + fillWrite(argList)  +"\n")
+                    print "User " + loggedIn.login.username + " executes " +argList[1] +" as " + fillWrite(argList)  +"\n"
+                else:
+                    auditFile.write("User " + loggedIn.login.username + " denied execute access of" + argList[1]+ "\n")
+                    print "User " + loggedIn.login.username + " denied execute access of" + argList[1]+ "\n"
             else:
-                print("Invalid arguments. Unrecognized command: " + line +"\n")
-                return
+                auditFile.write(
+                    "Invalid arguments or command never ran due to no user being logged in, invalid permissions, etc: " + line + "\n")
+                print(
+                            "Invalid arguments or command never ran due to no user being logged in, invalid permissions, etc: " + line + "\n")
+               # return
 
         elif argCount == 3:
             if argList[0] == "usergrp" and not loggedIn.logLock and (loggedIn.login.isRoot and checkUserExists(argList[1])) and checkGroupDup(argList[2]):
 
                 getGroup((argList[2])).userList.add(getUser(argList[1]))
-
                 #TODO
-                print("usergrp\n")
+                auditFile.write(
+                    "User " + argList[1] + " added to group " + argList[2] +"\n")
+                print "User " + argList[1] + " added to group " + argList[2] +"\n"
+
+
                 #return
             elif argList[0] == "useradd" and not loggedIn.logLock and (loggedIn.login.isRoot and not checkUserExists(argList[1]) and linecount > 1):
 
                 #TODO
                 user = User(False, argList[1], argList[2])
                 userListGlobal.add(user)
-                print("useradd\n")
+                auditFile.write("User " + argList[1] + " created "+"\n")
+                print "User " + argList[1] + " created "+"\n"
                 #return
             elif argList[0] == "chown" and not loggedIn.logLock and (loggedIn.login.isRoot and checkFileDup(argList[1]))\
                     and checkUserExists(argList[2]):
-                #tempPerms = getFile(argList[1]).perms1
+                tempPerms = getFile(argList[1]).perms1
 
                 getFile(argList[1]).owner = getUser(argList[2])
+                getFile(argList[1]).perms1 = tempPerms
+                auditFile.write("File " + argList[1] + " changed ownership of to  " + argList[2] + "\n")
+                print "File " + argList[1] + " changed ownership of to  " + argList[2] + "\n"
 
-                print("chown\n")
                 #TODO
                 #return
             elif argList[0] == "chgrp" and not loggedIn.logLock and checkGroupDup(argList[2]) and \
@@ -342,14 +395,16 @@ def makeCMDList(lines): #TODO make sure command to create root user is the first
 
                     getGroup(argList[2]).filePermMap[argList[1]] = getFile(argList[1]).perms2
                     file.groupName = argList[2]
-
+                    auditFile.write("File " + argList[1] + " changed group to  " + argList[2] + "\n")
+                    print "File " + argList[1] + " changed group to  " + argList[2] + "\n"
                 else:
                     #print 'boi2'
                     tempperms = getGroup(file.groupName).filePermMap[argList[1]]#temp swap to update new perms
                     getGroup(file.groupName).filePermMap[argList[1]] = "nil"
                     getGroup(argList[2]).filePermMap[argList[1]] = tempperms
                     file.groupName = argList[2]
-               # print("chgrp   "+ getGroup(argList[2]).filePermMap[argList[1]] +"\n")
+                    auditFile.write("File " + argList[1] + " changed group to  " + argList[2] + "\n")
+                    print "File " + argList[1] + " changed group to  " + argList[2] + "\n"
                 #TODO
                 #return
 
@@ -357,22 +412,30 @@ def makeCMDList(lines): #TODO make sure command to create root user is the first
             elif argList[0] == "login" and loggedIn.logLock:
 
                     if not checkUserExists(argList[1]):
-                            print "user no exists"
+                        auditFile.write(argList[1] + " tried to login. No account found"  +"\n")
+                        print argList[1] + " tried to login. No account found"  +"\n"
                     else:
                         if not getUser(argList[1]).password == argList[2]:
-                            print "bad pw"
+                            auditFile.write(argList[1] + " tried to login. Wrong Password!" + "\n")
+                            print argList[1] + " tried to login. Wrong Password!" + "\n"
                         else:
                             loggedIn.login = getUser(argList[1])
                             loggedIn.logLock = False
-                            print("login\n")
+                            auditFile.write(argList[1] + " logged in!" + "\n")
+                            print argList[1] + " logged in!" + "\n"
+
             elif(argList[0] == "login" and not loggedIn.logLock):
-                print"log out before in"
+                auditFile.write(argList[1] + " tried to login, but someone's already logged in!" + "\n")
+                print argList[1] + " tried to login, but someone's already logged in!" + "\n"
+
                 #TODO
 
                 #return
             else:
-                print("Invalid arguments. Unrecognized command: " + line + "\n")
-                return
+                auditFile.write(
+                    "Invalid arguments or command never ran due to no user being logged in, invalid permissions, etc: " + line + "\n")
+                print(
+                            "Invalid arguments or command never ran due to no user being logged in, invalid permissions, etc: " + line + "\n")
         elif argCount == 5:
 
             if argList[0] == "chmod" and not loggedIn.logLock and(checkFileDup(argList[1]) and (checkUserOwns(loggedIn.login.username, argList[1]) or
@@ -385,12 +448,13 @@ def makeCMDList(lines): #TODO make sure command to create root user is the first
                 file.perms3 = argList[4]
 
                 #TODO
-                print("chmod\n")
+                auditFile.write("Changed permissions of " + argList[1] + " to " +argList[2]+ " " + argList[3]+ " "+argList[4] + " by "+ loggedIn.login.username+"\n")
+                print "Changed permissions of " + argList[1] + " to " +argList[2]+ " " + argList[3]+ " "+argList[4] + " by "+ loggedIn.login.username+"\n"
                 #return
             else:
 
-                print("Invalid arguments. Unrecognized command: " + line + "\n")
-                return
+                auditFile.write("Invalid arguments or command never ran due to no user being logged in, invalid permissions, etc: " + line + "\n")
+                print("Invalid arguments or command never ran due to no user being logged in, invalid permissions, etc: " + line + "\n")
 
     return
         #keep going up to 5
